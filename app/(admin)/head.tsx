c16c716f3f731e79c92a26ea2af09000a160d7ba
mark choi <anhwms.official@gmail.com>
2026-01-29 17:22:58 +0900

    Mockup.ver1.05

--- app/(admin)/head.tsx
+++ app/(admin)/head.tsx
@@ -3,6 +3,10 @@ export default function Head() {
     <>
       <link
         rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;600;700&display=swap"
+      />
+      <link
+        rel="stylesheet"
         crossOrigin="anonymous"
         href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css"
       />
--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      style={{ fontFamily: '"Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif' }}
+      style={{ fontFamily: '"Noto Sans KR", "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif' }}
     >
       <Providers>
         <AuthProvider>
